3731c28dd78e2c9f7546fbc18f47e6cbe3169120
Priya Kumari <priyakumariyt05@gmail.com>
2024-04-13 14:48:16 +0530

    upload

---
+++ CamelCase.js
@@ -0,0 +1,13 @@
+const CamelCase = (str)=>{
+str = str.trim().split(" ")
+str = str.map((curElem, index)=>{
+    if(index===0){
+        return curElem.toLowerCase()
+    }else{
+        return curElem.charAt(0).toUpperCase() + curElem.slice(1).toLowerCase()
+    }
+})
+return str.join("")
+
+}
+console.log(CamelCase("my name is priya"))
