cf4dd581c71161169360188c4137a38a406350b8
Priya Kumari <priyakumariyt05@gmail.com>
2024-04-23 13:19:32 +0530

    upload

--- FactorialOfN.js
+++ FactorialOfN.js
@@ -1,10 +1,23 @@
+// Factorial in Recursion
+const FindFactorial = (num) =>{
+    if(num===1){
+        return 1     //base case
+    }else{
+        return num * FindFactorial(num-1)
+    }
+    
+}
+
+console.log(FindFactorial(5))
+
+// 2nd way
 const Factorial = (n)=>{
-let fact = 1
-for(let i=1; i<=n;i++){
-fact*= i
-}
-return fact
-}
-console.log(Factorial(5))
-console.log(Factorial(6))
-console.log(Factorial(0))
+    let fact = 1
+    for(let i=1; i<=n;i++){
+    fact*= i
+    }
+    return fact
+    }
+    console.log(Factorial(5))
+    console.log(Factorial(6))
+    console.log(Factorial(0))
