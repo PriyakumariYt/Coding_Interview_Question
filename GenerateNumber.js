aa16b4b4f003a8589f5d1dd74bcb8f0772e61055
Priya Kumari <priyakumariyt05@gmail.com>
2024-04-27 10:39:28 +0530

    upload

--- GenerateNumber.js
+++ GenerateNumber.js
@@ -1,9 +1,9 @@
 const GenerateNumberRange = (a,b)=>{
 let arr= []
-// while(a<=b){
-//     arr.push(a)
-//     a++
-// }
+while(a<=b){
+    arr.push(a)
+    a++
+}
 // 2nd way
 for(let i=a; i<=b;i++){
     arr.push(i)
@@ -12,3 +12,14 @@ return arr
 }
 console.log(GenerateNumberRange(0,7))
 console.log(GenerateNumberRange(-2,8))
+// 3rd way in recursive method 
+
+GenrateRecrusiveNumber = (a,b,arr=[]) =>{
+    // base case
+    if(a<=b){
+        arr.push(a)
+        return GenrateRecrusiveNumber(a + 1,b,arr)
+    }
+return arr
+}
+console.log(GenrateRecrusiveNumber(0,7))
--- SumOfArray.js
+++ SumOfArray.js
@@ -4,3 +4,12 @@ for(let i = 0; i<arr.length; i++){
 sum = sum + arr[i]
 }
 console.log(sum)
+
+// 2nd way in (...) operators
+const SumOfArray = (numbers) => {
+    return numbers.reduce((accum, curval) => {
+      return accum + curval;
+    }, 0);
+  }
+  
+  console.log(SumOfArray([2, 4, 6, 7, 5, 1, 2, 5, 12])); 
